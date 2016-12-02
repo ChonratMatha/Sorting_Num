74d2360fab1b27f12732887ea45befa049ddeb94
ChonratMatha <oiynoii@gmail.com>
2016-12-02 18:27:07 +0700

    test

---
+++ Sorting_Num/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Sorting_Num
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please input your Numbers :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(331, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(386, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(441, 54);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(49, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 22);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sort Min to Max";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 22);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sort Max to Min";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(141, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Keyword :";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(224, 135);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(127, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(366, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 22);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(155, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Result :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(224, 182);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(269, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(224, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(269, 22);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(202, 22);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "About";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sorting_Num.Properties.Resources.images;
+            this.ClientSize = new System.Drawing.Size(502, 260);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sortig_Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Sorting_Num/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting_Num
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] Nums = new double[5];  //ตั้ง array ที่มีการจำกัดจำนวนข้อมมูลมา 1 ตัว
+            Nums[0] = 0;                    //โดยใช้ตัวแปร Nums เก็บข้อมูลตั้งแต่ Indexที่ 0 ถึง 4
+            Nums[1] = 0;
+            Nums[2] = 0;
+            Nums[3] = 0;
+            Nums[4] = 0;
+            if (textBox1.Text == "")       //ตั้งเงื่อนไขถ้า Textbox ว่าง จะโชว์ข้อความที่กำหนดไว้
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+             if (textBox2.Text == "")     //ตั้งเงื่อนไขถ้า Textbox ว่าง จะโชว์ข้อความที่กำหนดไว้
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+             if (textBox3.Text == "")     //ตั้งเงื่อนไขถ้า Textbox ว่าง จะโชว์ข้อความที่กำหนดไว้
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox4.Text == "")     //ตั้งเงื่อนไขถ้า Textbox ว่าง จะโชว์ข้อความที่กำหนดไว้
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox5.Text == "")    //ตั้งเงื่อนไขถ้า Textbox ว่าง จะโชว์ข้อความที่กำหนดไว้
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            else                       //ถ้า textbox ไม่ว่าง จะทำตามเงื่อนไขนี้
+            {
+                Nums[0] = double.Parse(textBox1.Text);
+                Nums[1] = double.Parse(textBox2.Text);
+                Nums[2] = double.Parse(textBox3.Text);
+                Nums[3] = double.Parse(textBox4.Text);
+                Nums[4] = double.Parse(textBox5.Text);
+            }
+            Array.Sort(Nums); //ทำการเรียงข้อมูลใน Array
+            textBox1.Text = Convert.ToString(Nums[0]);
+            textBox2.Text = Convert.ToString(Nums[1]);
+            textBox3.Text = Convert.ToString(Nums[2]);
+            textBox4.Text = Convert.ToString(Nums[3]);
+            textBox5.Text = Convert.ToString(Nums[4]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+            //ปุ่มนี้จะเป็นการนำปุ่มแรกนั้น มาReverse หรือกลับค่าข้อมูล
+            //โดยเงื่อนไขการทำงานจะคล้ายปุ่มแรกและปุ่มที่สาม
+        {
+            double[] Nums = new double[5]; 
+            Nums[0] = 0;
+            Nums[1] = 0;
+            Nums[2] = 0;
+            Nums[3] = 0;
+            Nums[4] = 0;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox5.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            else
+            {
+                Nums[0] = double.Parse(textBox1.Text);
+                Nums[1] = double.Parse(textBox2.Text);
+                Nums[2] = double.Parse(textBox3.Text);
+                Nums[3] = double.Parse(textBox4.Text);
+                Nums[4] = double.Parse(textBox5.Text);
+            }
+            Array.Sort(Nums);
+            Array.Reverse(Nums);
+            textBox1.Text = Convert.ToString(Nums[0]);
+            textBox2.Text = Convert.ToString(Nums[1]);
+            textBox3.Text = Convert.ToString(Nums[2]);
+            textBox4.Text = Convert.ToString(Nums[3]);
+            textBox5.Text = Convert.ToString(Nums[4]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        //โดยเงื่อนไขการทำงานจะคล้ายปุ่มแรกและปุ่มที่สอง
+        //แต่เพิ่มความสามารถในการค้นหามา
+        {
+            double[] Nums = new double[5];
+            Nums[0] = 0;
+            Nums[1] = 0;
+            Nums[2] = 0;
+            Nums[3] = 0;
+            Nums[4] = 0;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            if (textBox5.Text == "")
+            {
+                MessageBox.Show(" Please check your Numbers ");
+            }
+            else
+            {
+                Nums[0] = double.Parse(textBox1.Text);
+                Nums[1] = double.Parse(textBox2.Text);
+                Nums[2] = double.Parse(textBox3.Text);
+                Nums[3] = double.Parse(textBox4.Text);
+                Nums[4] = double.Parse(textBox5.Text);
+            }
+            Array.Sort(Nums);
+            textBox1.Text = Convert.ToString(Nums[0]);
+            textBox2.Text = Convert.ToString(Nums[1]);
+            textBox3.Text = Convert.ToString(Nums[2]);
+            textBox4.Text = Convert.ToString(Nums[3]);
+            textBox5.Text = Convert.ToString(Nums[4]);
+            if (textBox6.Text == ""){                    //ตั้งเงื่อนไขเช็คtextbox6  ถ้าว่างให้แสดงข้อความแจ้งเตือนผู้ใช้
+            MessageBox.Show(" Please input the data that you ned to find!! "); }
+            if (textBox6.Text == textBox1.Text){       //ตั้งเงื่อนไขเช็คtextbox6  ถ้าตรงกับtextboxใด ก็จะเเสดงผลการค้นหา
+            textBox7.Text = " At Index: 0 Collum: 1"; }
+            else if (textBox6.Text == textBox2.Text) {
+            textBox7.Text = " At Index: 1 Collum: 2"; }
+            else if (textBox6.Text == textBox3.Text) {
+            textBox7.Text = " At Index: 2 Collum: 3"; }
+            else if (textBox6.Text == textBox4.Text) {
+            textBox7.Text = " At Index: 3 Collum: 4"; }
+            else if (textBox6.Text == textBox5.Text) {
+            textBox7.Text = " At Index: 4 Collum: 5"; }
+            else {                                        //ถ้าไม่เจอจะเเสดงข้อความแจ้งเตือน
+            MessageBox.Show(" err 404 Not Found!! "); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();      //ใช้ปิดหน้าต่าง
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            About f2 = new About(); //แสดงหน้าต่างที่สองหลังจากกดปุ่ม
+            f2.Show();
+        }
+      }
+    }
+
